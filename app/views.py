000afdb8d437cdb8faa8a32164b54446cd46da59
sivanandana <sivanandana2001@gmail.com>
2023-06-01 14:02:38 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def siva(request):
+    return render(request,'siva.html')
+def gowtham(request):
+    return render(request,'gowtham.html')
